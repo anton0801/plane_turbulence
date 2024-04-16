7433e1d36abaa4f1b7bcc7a96a9c25b51e938b0b
Anton <danilovanton724@gmail.com>
2024-04-16 15:09:19 +0200

    Initial Commit

---
+++ Plane Turbulence/Plane_TurbulenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plane_TurbulenceApp.swift
+//  Plane Turbulence
+//
+//  Created by Anton on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Plane_TurbulenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
